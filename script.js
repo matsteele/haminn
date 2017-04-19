026a00f067e4a4b8bf908b9f32590d875e8dbc78
studellConsulting <studellConsulting@gmail.com>
2017-04-19 16:16:42 -0400

    adapted to other browsers

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// $(document).ready(function(){
-// 	 $('#fourOne').click(function(){
-// 	   $("#openRoom4_1").modal();
+
+
+$(document).ready(function(){
+	 $('.roomSVGs').click(function(){
+    var id = $(this).attr('class');
+    console.log(id);
+    IDArray = id.split(" ");
+    console.log(IDArray[0]);
+     var link = 'index.html#' + IDArray[0];
+     console.log(link);
+      $(location).attr('href', link);
+    });
+	});
+
+
+
+
+// });
+
 //
+// var  triangle = document.getElementById("fourOne");
 //
-//     });
-// 	});
-// // });
-
-
-var  triangle = document.getElementById("fourOne");
-
-triangle.addEventListener('click', function(evt) {
-    $('#openRoom4_1').modal('show');
-});
+// triangle.addEventListener('click', function() {
+//     $('#openRoom4_1').;
+// });
