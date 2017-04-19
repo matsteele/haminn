2dc909789e9724fb54fc5ed54a981e3d56b9c15e
studellConsulting <studellConsulting@gmail.com>
2017-04-19 15:18:03 -0400

    added script

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // $(document).ready(function(){
 // 	 $('#fourOne').click(function(){
 // 	   $("#openRoom4_1").modal();
-
-// $('#openModal1').bind('click', function(event){
-//     if (event.target == $('#openModal1').get(0))
-//         overlay();
-// });
-// 
-// });
-// // 	});
+//
+//
+//     });
+// 	});
 // // });
+
+
+var  triangle = document.getElementById("fourOne");
+
+triangle.addEventListener('click', function(evt) {
+    $('#openRoom4_1').modal('show');
+});
